246b6c2f991438d942cef9fe5b69b12cf26db4b6
meena <mennups301@gmail.com>
2024-12-05 21:09:33 +0530

    prgm5

---
+++ prgm5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class prgm5{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int num = sc.nextInt();
+int x;
+    for(int i=0; i<=1000; i=i+100)
+    {
+        System.out.println(i);
+    }
+}
+}
