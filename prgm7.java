2e51a1cb815cec3c39352b69c662139c4bea50ba
meena <mennups301@gmail.com>
2024-12-05 22:01:11 +0530

    code7

--- prgm7.java
+++ prgm7.java
@@ -6,7 +6,6 @@ class prgm7
         Scanner sc=new Scanner(System.in);
         int num = sc.nextInt();
         int num1= sc.nextInt();
-
         for (int i=1; i<=num1; i++)
         {
             System.out.println(num);
