76a21a5b30ff462fb30b0535d6ebcff4b53ee1c9
meena <mennups301@gmail.com>
2024-12-08 07:14:02 +0530

    code

---
+++ contestcode1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class contestcode1
+{
+public static void main(String []args)
+{
+Scanner sc =new Scanner(System.in);
+int num=sc.nextInt();
+for(int i=2; i<=num; i++)
+{
+System.out.println(i);
+}
+}
+}
