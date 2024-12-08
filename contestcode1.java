4dbdfb14f60e303f77cc2fcd980e90dcea6fd747
meena <mennups301@gmail.com>
2024-12-08 07:15:59 +0530

    edited

--- contestcode1.java
+++ contestcode1.java
@@ -5,7 +5,7 @@ public static void main(String []args)
 {
 Scanner sc =new Scanner(System.in);
 int num=sc.nextInt();
-for(int i=2; i<=num; i++)
+for(int i=3; i<=num; i++)
 {
 System.out.println(i);
 }
