8446fb76001c1209cc14be52d0e3d3c8519526cb
meena <mennups301@gmail.com>
2024-12-08 07:34:44 +0530

    code

---
+++ contestcode2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class contestcode2
+{
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int a=0;
+        for(int i=1; i<=num; i++)
+        {
+            a=a+i;
+        }
+        {
+            System.out.println(a);
+        }
+    }
+}
---
+++ contestcode3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class contestcode3
+{
+public static void main(String [] args)
+{
+Scanner sc= new Scanner(System.in);
+int num=sc.nextInt();
+char letter=sc.next().charAt(0);
+float num1=sc.nextFloat();
+sc.nextLine();
+String Name=sc.nextLine();
+{
+System.out.println(num);
+System.out.println(letter);
+System.out.println(num1);
+System.out.println(Name);
+}
+}
+}
