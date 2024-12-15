1695fd382958b222902107a213ad9741867bb11b
meena <mennups301@gmail.com>
2024-12-15 12:54:29 +0530

    changes

---
+++ patterncode14.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class patterncode14
+{
+    public static void main (String [] args)
+    {
+        int n=8;
+        int count =1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(count);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+           count++;
+        }
+    }
+}
